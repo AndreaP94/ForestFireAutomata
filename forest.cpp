50cbe56e9ba729d8ee3d90d2551ad2c01dbe615f
Sparq94 <pasqualearieta94@gmail.com>
2019-07-03 18:34:22 +0200

    Add files via upload
    Fixed Sequential Neighbourds control

--- forest.cpp
+++ forest.cpp
@@ -8,20 +8,38 @@
 
 using namespace std;
 
-//enum wind {NW = 0, N = 1, NE = 2, E = 3, SE = 4, S = 5, SW = 6, W = 7};
+/**The wind is encoded w.r.t it's direction as:
+    North West (NW) = 0
+    North(N) = 1
+    North East(NE) = 2
+    East(E) = 3
+    South East(SE) = 4
+    South(S) = 5
+    South West(SW) = 6
+    West(W) = 7
+**/
+
+/**The fuel represent the combustion type, in particular dark green, green, light green**/
 enum fuel {VS = 0, VM = 1, VC = 2};
 
+/**encoding of the wind type**/
 vector<string> wind {"NW", "N", "NE", "E", "SE", "S", "SW", "W"};
+
+/**Each combustion type has an associeted value of burn**/
 const int burn[3] = {33, 66, 99};
 
+/**Probability that a lighting fall on a combustion cell**/
 const float lighting = 0.00002;
+
+/**Probability that in an empty cell born a combustion type**/
 const float growth = 0.002;
 
-const int width = 15;
-const int height = 10;
+/**Size of the forest**/
+const int width = 5;
+const int height = 5;
 
-string directionWind = wind[0];
-int indexDirectionWind = 0;
+int indexDirectionWind = 1;
+string directionWind = wind[indexDirectionWind];
 int currentIteration = 0;
 
 map <string, vector<pair<int, int>>> windNeighbourds = {
@@ -35,21 +53,15 @@ map <string, vector<pair<int, int>>> windNeighbourds = {
 	{wind[7], { {-1, 1}, {0, 1}, {1, 1} } }
 };
 
-
-
-
 bool struckByLightning() {
 	const float min = lighting/2;
 	const float max = growth/2;
 
 	float range = max - min;
-
 	float random = range * (static_cast <float> (rand()) / static_cast <float> (RAND_MAX)) + min;
 
 	if(random < lighting)
 		return true;
-		//cout << "Number = " << random << "\n";
-
 	return false;
 }
 bool fuelGrowth() {
@@ -62,8 +74,6 @@ bool fuelGrowth() {
 
 	if(random < growth)
 		return true;
-		//cout << "Number = " << random << "\n";
-
 	return false;
 }
 
@@ -87,9 +97,9 @@ void copyMatrix(int** currentGeneration, int** nextGeneration) {
 void printMatrix(int** m) {
   for (int i = 0; i < width; i++) {
     for (int j = 0; j < height; j++) {
-      cout << m[i][j] << " ";
+      cout << m[i][j] << "      ";
     }
-    cout << "\n";
+    cout << endl;
   }
 }
 
@@ -97,11 +107,14 @@ void transitionFunction(int** currentGeneration, int** nextGeneration) {
 
 	for (int i = 0; i < width; i++) {
 		for (int j = 0; j < height; j++) {
+            
+            //Born of fuel into an empty cell
 			if(currentGeneration[i][j] == -1) {
 				if(fuelGrowth()) {
 					nextGeneration[i][j] = fuel(rand() % 3);
 				}
 			}
+            //Fuel cell (1. Struck by lighting. 2. One of its neighbourds burn and i am in the same wind direction
 			else if(currentGeneration[i][j] >= 0 && currentGeneration[i][j] <= 2) {
 				if(struckByLightning()) {
 					nextGeneration[i][j] = burn[currentGeneration[i][j]];
@@ -114,24 +127,22 @@ void transitionFunction(int** currentGeneration, int** nextGeneration) {
 							int Nrow = i + neighbourds[z].first;
 							int Ncolumn = j + neighbourds[z].second;
 
-							if(Nrow < 0){
+							if(Nrow < 0)
 								Nrow = width - 1;
-							}
-							if(Ncolumn < 0){
+							if(Ncolumn < 0)
 								Ncolumn = height - 1;
-							}
-							if(Nrow < 0 || Nrow >= width || Ncolumn < 0 || Ncolumn >= height)
-								cout << " SONO QUI";
-								else
-							if(currentGeneration[Nrow][Ncolumn] > 33){
+					        if(Nrow >= width)
+                                Nrow = 0;   
+                            if(Ncolumn >= height)
+                                Ncolumn = 0;
+
+							if(currentGeneration[Nrow][Ncolumn] >= 33){
+                                cout << "FOUND: iter: " << currentIteration <<" my_pos:  "<< i << " " << j << " neigh_pos: " << Nrow << " " << Ncolumn << endl;
 								nextGeneration[i][j] = burn[currentGeneration[i][j]];
 								break;
 							}
 					}
-
-
 				}
-				//controllo sui vicini
 			}
 			else {
 				if(currentGeneration[i][j] < burn[2])
@@ -169,11 +180,11 @@ int main() {
 
 	for (int i = 0; i < 20; i++) {
 
-		if(currentIteration % 2 != 0)
-			changeWindDirection();
+		//if(currentIteration % 2 != 0)
+			//changeWindDirection();
 
 		cout << currentIteration << " WIND = " << wind[indexDirectionWind] << '\n';
-		//printMatrix(currentGeneration);
+		printMatrix(currentGeneration);
 		transitionFunction(currentGeneration, nextGeneration);
 		copyMatrix(currentGeneration, nextGeneration);
 		//swapMatrix(&currentGeneration, &nextGeneration);
@@ -182,10 +193,7 @@ int main() {
 		currentIteration++;
 	}
 
-
-
-
-	//printMatrix(currentGeneration);
+	printMatrix(currentGeneration);
 	/*
 	cout << "\n";
 	printMatrix(nextGeneration);
